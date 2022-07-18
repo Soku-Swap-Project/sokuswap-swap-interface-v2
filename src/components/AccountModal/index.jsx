229bed84ecccf55c9816db80cccf50e07ab2a49a
ChefMari <demaricameron@outlook.com>
2022-07-17 20:29:28 -0400

    ui updates

--- src/components/AccountModal/index.jsx
+++ src/components/AccountModal/index.jsx
@@ -2,95 +2,107 @@
 
 import Modal from '@material-ui/core/Modal'
 import { makeStyles } from '@material-ui/core/styles'
-import CloseIcon from '@mui/icons-material/Close';
-import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import CloseIcon from '@mui/icons-material/Close'
+import OpenInNewIcon from '@mui/icons-material/OpenInNew'
 import { getExplorerLink } from 'app/functions'
 import { useActiveWeb3React } from 'app/services/web3'
 import React from 'react'
 
 /* eslint-disable */
 
-const useStyles = makeStyles(theme => ({
-    paper: {
-        position: 'absolute',
-        backgroundColor: theme.palette.background.paper,
-        border: '2px solid #000',
-        boxShadow: theme.shadows[10],
-        padding: theme.spacing(2, 4, 3)
-    }
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[10],
+    padding: theme.spacing(2, 4, 3),
+  },
 }))
 
 export default function AccountModal() {
-    const classes = useStyles()
-    const [open, setOpen] = React.useState(false)
-    // const { login, logout } = useAuth()
-    const { account, chainId } = useActiveWeb3React()
+  const classes = useStyles()
+  const [open, setOpen] = React.useState(false)
+  // const { login, logout } = useAuth()
+  const { account, chainId } = useActiveWeb3React()
 
-    const truncatedFirstHalf = account?.substring(0, 5)
-    const truncatedLastHalf = account?.substring(account.length - 5, account.length)
-    const truncatedAddress = `${truncatedFirstHalf}...${truncatedLastHalf}`
+  const truncatedFirstHalf = account?.substring(0, 5)
+  const truncatedLastHalf = account?.substring(account.length - 5, account.length)
+  const truncatedAddress = `${truncatedFirstHalf}...${truncatedLastHalf}`
 
-        const isMobile = window.innerWidth <= 1000
+  const isMobile = window.innerWidth <= 1200
 
+  const handleOpen = () => {
+    setOpen(true)
+  }
 
-    const handleOpen = () => {
-        setOpen(true)
-    }
+  const handleClose = () => {
+    setOpen(false)
+  }
 
-    const handleClose = () => {
-        setOpen(false)
-    }
+  // const logoutAccount = () => {
+  //     localStorage.removeItem('redux_localstorage_simple_user')
+  //     return deactivate
+  // }
 
-    // const logoutAccount = () => {
-    //     localStorage.removeItem('redux_localstorage_simple_user')
-    //     return deactivate
-    // }
+  const body = (
+    <div className="flex flex-col gap-6 network_modal">
+      <div className="modal_header">
+        <h1 className="text-blue font-bold">Account Details</h1>
+        <CloseIcon
+          className="hover_shadow_icon"
+          style={{ color: '#05195a', cursor: 'pointer' }}
+          onClick={handleClose}
+        />
+      </div>
+      <hr />
+      <div className="account__modal_details">
+        <div className="wallet_info">
+          <p style={{ fontSize: '16px', fontWeight: 'bold' }}>Wallet: {truncatedAddress}</p>
+          <img
+            className="nav_logo"
+            alt="Logo"
+            src="https://bscscan.com/token/images/sokuv2_32.png"
+            style={{ height: '20px', marginLeft: '5px' }}
+          />
+        </div>
 
-    const body = (
-        <div className="flex flex-col gap-6 network_modal">
-            <div className="modal_header">
-            <h1 className="text-blue font-bold">Account Details</h1>
-                <CloseIcon className='hover_shadow_icon' style={{color: '#05195a', cursor: 'pointer'}} onClick={handleClose} />
-            </div>
-            <hr />
-            <div className="account__modal_details">
-                <div className="wallet_info">
-                    <p style={{ fontSize: '16px', fontWeight: 'bold' }}>Wallet: {truncatedAddress}</p>
-                    <img
-                        className="nav_logo"
-                        alt="Logo"
-                        src="https://bscscan.com/token/images/sokuv2_32.png"
-                        style={{ height: '20px', marginLeft: '5px' }}
-                    />
-                </div>
-
-                <a target="_blank" className="view_on_scan hover_shadow" style={{ color:'#fff', background: '#05195a', padding: '12px 24px', borderRadius: '14px'}} href={getExplorerLink(chainId, account, 'address')}>
-                    <h2 className='pr-2'>View on Etherscan</h2>
-                    <OpenInNewIcon />
-                </a>
-                {/* <button className="account_logout" onClick={logoutAccount()}>
+        <a
+          target="_blank"
+          className="view_on_scan hover_shadow"
+          style={{ color: '#fff', background: '#05195a', padding: '12px 24px', borderRadius: '14px' }}
+          href={getExplorerLink(chainId, account, 'address')}
+        >
+          <h2 className="pr-2">View on Etherscan</h2>
+          <OpenInNewIcon />
+        </a>
+        {/* <button className="account_logout" onClick={logoutAccount()}>
                     <h2>Log Out</h2>
                     <span className="material-icons ">logout</span>
                 </button> */}
-            </div>
-        </div>
-    )
+      </div>
+    </div>
+  )
 
-    return (
-        <>
-            <li type="button" className={isMobile ? 'account_modal_mobile' : 'account_modal' + " hover_shadow p-3"} onClick={handleOpen}>
-                <span>Account:</span>
-                {truncatedAddress}
-            </li>
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="simple-modal-title"
-                aria-describedby="simple-modal-description"
-                className="network_modal_container"
-            >
-                {body}
-            </Modal>
-        </>
-    )
+  return (
+    <>
+      <li
+        type="button"
+        className={isMobile ? 'account_modal_mobile' : 'account_modal' + ' hover_shadow p-3'}
+        onClick={handleOpen}
+      >
+        <span>Account:</span>
+        {truncatedAddress}
+      </li>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        className="network_modal_container"
+      >
+        {body}
+      </Modal>
+    </>
+  )
 }
--- src/components/ClaimSokuModal/index.jsx
+++ src/components/ClaimSokuModal/index.jsx
@@ -2,66 +2,70 @@
 
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import CloseIcon from '@mui/icons-material/Close';
+import CloseIcon from '@mui/icons-material/Close'
 import Modal from '@material-ui/core/Modal'
 import { useActiveWeb3React } from 'app/services/web3'
 
 // import './ClaimSoku.css'
 
-const useStyles = makeStyles(theme => ({
-    paper: {
-        position: 'absolute',
-        backgroundColor: theme.palette.background.paper,
-        border: '2px solid #000',
-        boxShadow: theme.shadows[10],
-        padding: theme.spacing(2, 4, 3)
-    }
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: 'absolute',
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[10],
+    padding: theme.spacing(2, 4, 3),
+  },
 }))
 
 export default function ClaimSokuModal() {
-    const classes = useStyles()
-    const [open, setOpen] = React.useState(false)
-    const { account } = useActiveWeb3React()
+  const classes = useStyles()
+  const [open, setOpen] = React.useState(false)
+  const { account } = useActiveWeb3React()
 
-    const handleOpen = () => {
-        setOpen(true)
-    }
+  const handleOpen = () => {
+    setOpen(true)
+  }
 
-    const handleClose = () => {
-        setOpen(false)
-    }
+  const handleClose = () => {
+    setOpen(false)
+  }
 
-    const isMobile = window.innerWidth <= 850
+  const isMobile = window.innerWidth <= 1200
 
-    const date = new Date()
-    const month = date.getMonth() + 1
-    const day = date.getDate() + 7
-    const year = date.getFullYear()
+  const date = new Date()
+  const month = date.getMonth() + 1
+  const day = date.getDate() + 7
+  const year = date.getFullYear()
 
-    const earnedAmount = 50
+  const earnedAmount = 50
 
-    const body = (
-        <div className="flex flex-col gap-6 network_modal">
-        <div className="modal_header">
-            <h1 className="text-blue font-bold">Claim SOKU</h1>
-                <CloseIcon className='hover_shadow_icon' style={{color: '#05195a', cursor: 'pointer'}} onClick={handleClose} />
-            </div>
-            <hr />
-            {account ? (
-                <div>
-                    {/* <h2 id="simple-modal-title">Your Rewards:</h2>
+  const body = (
+    <div className="flex flex-col gap-6 network_modal">
+      <div className="modal_header">
+        <h1 className="text-blue font-bold">Claim SOKU</h1>
+        <CloseIcon
+          className="hover_shadow_icon"
+          style={{ color: '#05195a', cursor: 'pointer' }}
+          onClick={handleClose}
+        />
+      </div>
+      <hr />
+      {account ? (
+        <div>
+          {/* <h2 id="simple-modal-title">Your Rewards:</h2>
           <br />
           <p id="simple-modal-description">
             Congratulations! You've earned <strong className="soku_rewarded">{earnedAmount} SOKU</strong> tokens this
             week.
           </p> */}
-                    <h2 id="simple-modal-title">Rewards:</h2>
-                    <br />
-                    <p id="simple-modal-description">No rewards at this time.</p>
-                </div>
-            ) : (
-                <div className="claimSoku__noRewards">
-                    {/* <h2 id="simple-modal-title">No Rewards:</h2>
+          <h2 id="simple-modal-title">Rewards:</h2>
+          <br />
+          <p id="simple-modal-description">No rewards at this time.</p>
+        </div>
+      ) : (
+        <div className="claimSoku__noRewards">
+          {/* <h2 id="simple-modal-title">No Rewards:</h2>
           <br />
           <p id="simple-modal-description">{`You do not qualify for rewards at this time. `}</p>
           <p id="simple-modal-description">{`Please check back on: ${month}/${day}/${year}`}</p>
@@ -69,33 +73,38 @@ export default function ClaimSokuModal() {
           <p id="simple-modal-description">Click the link below to see how to get your rewards!</p>
           <br />
           <a href="#">Get Rewards</a> */}
-                    <h2 id="simple-modal-title">Rewards:</h2>
-                    <br />
-                    <p id="simple-modal-description">Please connect your wallet to view your rewards.</p>
-                </div>
-            )}
+          <h2 id="simple-modal-title">Rewards:</h2>
+          <br />
+          <p id="simple-modal-description">Please connect your wallet to view your rewards.</p>
         </div>
-    )
+      )}
+    </div>
+  )
 
-    return (
-        <div className="claimSoku__wrapper">
-            <button type="button" className={isMobile ? 'claimSoku__navButton_mobile' : 'claimSoku__navButton' + " hover_shadow"} style={{ background: '#05195a', padding: '12px 24px', whiteSpace: 'nowrap', fontWeight: 700}} onClick={handleOpen}>
-                Claim Soku
-            </button>
-            {/* <a href="https://tokensale.sokuswap.finance/token-exchange/#/" target="_blank">
+  return (
+    <div className="claimSoku__wrapper">
+      <button
+        type="button"
+        className={isMobile ? 'claimSoku__navButton_mobile' : 'claimSoku__navButton' + ' hover_shadow'}
+        style={{ background: '#05195a', padding: '12px 24px', whiteSpace: 'nowrap', fontWeight: 700 }}
+        onClick={handleOpen}
+      >
+        Claim Soku
+      </button>
+      {/* <a href="https://tokensale.sokuswap.finance/token-exchange/#/" target="_blank">
         <button type="button" className="claimSoku__navButton">
           Claim Soku
         </button>
       </a> */}
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="simple-modal-title"
-                aria-describedby="simple-modal-description"
-                className="network_modal_container"
-            >
-                {body}
-            </Modal>
-        </div>
-    )
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+        className="network_modal_container"
+      >
+        {body}
+      </Modal>
+    </div>
+  )
 }
