36e01e34465a6dd98a134883856b4df6b5cf1f0b
ChefMari <demaricameron@outlook.com>
2022-07-26 16:16:46 -0400

    feat: test

--- scripts/wait-for-hardhat.sh
+++ scripts/wait-for-hardhat.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-GET_BLOCKHAIN() {
-    return $(curl --write-out '%{http_code}' --silent --output /dev/null http://localhost:8545)
-}
+# GET_BLOCKHAIN() {
+#     return $(curl --write-out '%{http_code}' --silent --output /dev/null http://localhost:8545)
+# }
 
-GET_BLOCKHAIN
-status=$?
+# GET_BLOCKHAIN
+# status=$?
 
-until [ $status == 200 ]
-do
-    echo "Blockchain is unavailable..."
-    GET_BLOCKHAIN
-    status=$?
-    sleep 2
-done
+# until [ $status == 200 ]
+# do
+#     echo "Blockchain is unavailable..."
+#     GET_BLOCKHAIN
+#     status=$?
+#     sleep 2
+# done
 
-echo "Blockchain detected!"
+# echo "Blockchain detected!"
