ea1b00d7a2960addf7722f11f9656771c65765db
ChefMari <demaricameron@outlook.com>
2022-07-26 18:34:48 -0400

    feat: https to wss

--- src/connectors/web3Providers.ts
+++ src/connectors/web3Providers.ts
@@ -2,10 +2,11 @@ import { SupportedChainId } from 'app/enums/SupportedChainId'
 import Web3 from 'web3'
 
 let EthProvider: any
-
 export const getEthProvider = (): any => {
   if (!EthProvider) {
-    EthProvider = new Web3.providers.HttpProvider(process.env.NEXT_PUBLIC_ETH_RPC as any)
+    // EthProvider = new Web3.providers.HttpProvider(process.env.NEXT_PUBLIC_ETH_RPC as any)
+    EthProvider = new Web3.providers.WebsocketProvider(process.env.NEXT_PUBLIC_ETH_RPC_WSS as string)
+    // EthProvider = new Web3HttpProvider(process.env.NEXT_PUBLIC_ETH_RPC, options)
   }
   return EthProvider
 }
@@ -13,9 +14,10 @@ export const getEthProvider = (): any => {
 let BSCProvider: any
 export const getBSCProvider = (): any => {
   if (!BSCProvider) {
-    BSCProvider = new Web3.providers.HttpProvider(
-      process.env.NEXT_PUBLIC_BSC_RPC || 'https://bsc-dataseed.binance.org/'
-    )
+    // BSCProvider = new Web3.providers.HttpProvider(
+    //   process.env.NEXT_PUBLIC_BSC_RPC || 'https://bsc-dataseed.binance.org/'
+    // )
+    BSCProvider = new Web3.providers.WebsocketProvider(process.env.NEXT_PUBLIC_BSC_RPC_WSS as string)
   }
   return BSCProvider
 }
@@ -23,7 +25,8 @@ export const getBSCProvider = (): any => {
 let PolygonProvider: any
 export const getPolygonProvider = (): any => {
   if (!PolygonProvider) {
-    PolygonProvider = new Web3.providers.HttpProvider(process.env.NEXT_PUBLIC_POLYGON_RPC as any)
+    // PolygonProvider = new Web3.providers.HttpProvider(process.env.NEXT_PUBLIC_POLYGON_RPC as any)
+    PolygonProvider = new Web3.providers.WebsocketProvider(process.env.NEXT_PUBLIC_POLYGON_RPC_WSS as string)
   }
   return PolygonProvider
 }
@@ -31,7 +34,8 @@ export const getPolygonProvider = (): any => {
 let AvalancheProvider: any
 export const getAvalancheProvider = (): any => {
   if (!AvalancheProvider) {
-    AvalancheProvider = new Web3.providers.HttpProvider(process.env.NEXT_PUBLIC_AVALANCHE_RPC as any)
+    // AvalancheProvider = new Web3.providers.HttpProvider(process.env.NEXT_PUBLIC_AVALANCHE_RPC as any)
+    AvalancheProvider = new Web3.providers.WebsocketProvider(process.env.NEXT_PUBLIC_AVALANCHE_RPC_WSS as string)
   }
   return AvalancheProvider
 }
@@ -39,35 +43,40 @@ export const getAvalancheProvider = (): any => {
 let MoonRiverProvider: any
 export const getMoonRiverProvider = (): any => {
   if (!MoonRiverProvider) {
-    MoonRiverProvider = new Web3.providers.HttpProvider(process.env.NEXT_PUBLIC_MOONRIVER_RPC as any)
+    // MoonRiverProvider = new Web3.providers.HttpProvider(process.env.NEXT_PUBLIC_MOONRIVER_RPC as any)
+    MoonRiverProvider = new Web3.providers.WebsocketProvider(process.env.NEXT_PUBLIC_MOONRIVER_RPC_WSS as string)
   }
   return MoonRiverProvider
 }
 let FantomProvider: any
 export const getFantomProvider = (): any => {
   if (!FantomProvider) {
-    FantomProvider = new Web3.providers.HttpProvider(process.env.NEXT_PUBLIC_FANTOM_RPC as any)
+    // FantomProvider = new Web3.providers.HttpProvider(process.env.NEXT_PUBLIC_FANTOM_RPC as any)
+    FantomProvider = new Web3.providers.WebsocketProvider(process.env.NEXT_PUBLIC_FANTOM_RPC_WSS as string)
   }
   return FantomProvider
 }
 let HarmonyProvider: any
 export const getHarmonyProvider = (): any => {
   if (!HarmonyProvider) {
-    HarmonyProvider = new Web3.providers.HttpProvider(process.env.NEXT_PUBLIC_HARMONY_RPC as any)
+    // HarmonyProvider = new Web3.providers.HttpProvider(process.env.NEXT_PUBLIC_HARMONY_RPC as any)
+    HarmonyProvider = new Web3.providers.WebsocketProvider(process.env.NEXT_PUBLIC_HARMONY_RPC_WSS as string)
   }
   return HarmonyProvider
 }
 let ArbitrumProvider: any
 export const getArbitrumProvider = (): any => {
   if (!ArbitrumProvider) {
-    ArbitrumProvider = new Web3.providers.HttpProvider(process.env.NEXT_PUBLIC_ARBITRUM_RPC as any)
+    // ArbitrumProvider = new Web3.providers.HttpProvider(process.env.NEXT_PUBLIC_ARBITRUM_RPC as any)
+    ArbitrumProvider = new Web3.providers.WebsocketProvider(process.env.NEXT_PUBLIC_ARBITRUM_RPC_WSS as string)
   }
   return ArbitrumProvider
 }
 let AuroraProvider: any
 export const getAuroraProvider = (): any => {
   if (!AuroraProvider) {
-    AuroraProvider = new Web3.providers.HttpProvider(process.env.NEXT_PUBLIC_AURORA_RPC as any)
+    // AuroraProvider = new Web3.providers.HttpProvider(process.env.NEXT_PUBLIC_AURORA_RPC as any)
+    AuroraProvider = new Web3.providers.WebsocketProvider(process.env.NEXT_PUBLIC_AURORA_RPC_WSS as string)
   }
   return AuroraProvider
 }
